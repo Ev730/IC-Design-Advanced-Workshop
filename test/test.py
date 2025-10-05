0090c6e10616d8b2ab9e8e7585b1b9b31292467f
Aur0ra <112654882+Ev730@users.noreply.github.com>
2025-10-05 15:41:59 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    clock = Clock(dut.clk, 10, unit="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -20,21 +20,17 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 2)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    await RisingEdge(dut.clk)
+    assert int(dut.uo_out.value) == 0, f"Expected 0, got {int(dut.uo_out.value)}"
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Check a few values
+    for expected in range(1, 6);
+        await RisingEdge(dut.clk)
+        got = int(dut.uo_out.value)
+        assert got == expected, f"Expected {expected}, got {got}"
+        dut._log.info(f"Counter Value = {got}")
